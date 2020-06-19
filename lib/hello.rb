c6de310dac9d452f3e495cdc8f190b685e614bf3
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-19 18:53:17 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -21,9 +21,11 @@ def hello_t(array)
     while i < array.length
       yield(array[i])
       i = i + 1
-  array
-   else puts "Hey! No block was given!"
-  end
-end 
+    end
 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end
  
